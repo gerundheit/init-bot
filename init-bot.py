e6fa3126154a2fda652a2501a4943be02c37c1b5
Ray Stevens <ray@rastevens.me>
2021-04-17 09:05:39 -0400

    Logic for adding characters to list plus simplified file structure

--- init-bot.py
+++ init-bot.py
@@ -3,20 +3,23 @@ import os
 
 import discord
 from dotenv import load_dotenv
-from calcs import d20
+from random import randint
 
 #Tokens are stored in .env.
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+initiative_entries = []
+
+#Basic die roller
+def d20():
+    return randint(1, 20)
 
 #Confirms in shell that bot is connected to the server
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
-initiative_entries = {}
 
 #Prompts that will cause Init-Bot to respond to text messages in channels.
 @client.event
@@ -27,19 +30,25 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-    #Add a character to those getting their initiatives rolled
+    #Add a character to those getting their initiatives rolled by appending their name and other arguments to the initiative list as a tuple.
     if message.content.startswith('$i'):
-        await message.channel.send('I understand you\'re opening combat.')
+        command = str(message.content).split()
+        try:
+            initiative_entries.append((command[1], command[2], command[3]))
+        except:
+            initiative_entries.append((command[1], command[2]))
+        await message.channel.send('Added {} to combat.'.format(command[1]))
+        await message.channel.send(initiative_entries)
 
     #Rolls initiative for all characters entered, creates a new dictionary of chracters and roll values, and posts/pins post
     if message.content.startswith('$roll'):
         await message.channel.send('Here\'s the part where I roll initiative.')
 
-    #Allows users to insert late-coming characters in the middle of combat, either with a pre-specified iniaitive roll, or rolling for them.
+    #Allows users to insert late-coming characters in the middle of combat, either with a pre-specified iniaitive roll, or rolling for them. [Note: rolling for them not yet implemented.]
     #if message.content.startswith('$new-challenger'):
         #await.message.channel.send('Gotcha, inserting a new guy!')
 
-    #Clears the initiative order in the program and unpins the previous message for a fresh start
+    #Clears the initiative order from the list variable and unpins the previous message for a fresh start
     if message.content.startswith('$end'):
         await message.channel.send('I understand combat has ended.')
 
@@ -48,7 +57,7 @@ async def on_message(message):
 
         $hello — I'll say hello back!
 
-        $i — Command to add someone to my initiative tracker list. After the $i, specify character name, iniative bonus, and whether they get advantage on the roll (if they do, say "adv"; if they don't, you don't need to say so.) Example: "$i Fiver adv" means that Fiver gets +5 to initiative rolls and has advantage on initiative rolls; "$i Fiver 5" means she has +5 but no initiative. Enter this command once per character, per combat.
+        $i — Command to add someone to my initiative tracker list. After the $i, specify character name, initiative bonus, and whether they get advantage on the roll (if they do, say "adv"; if they don't, you don't need to say so.) Example: "$i Fiver adv" means that Fiver gets +5 to initiative rolls and has advantage on initiative rolls; "$i Fiver 5" means she has +5 but no advantage. Enter this command once per character, per combat.
 
         $roll — Command to have me take all of the characters you've entered, roll their initiative for you, and put them in order. I'll pin the post to make it easy to reference during combat.
 
