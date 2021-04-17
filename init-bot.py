3c85f524ece063db3607b5f53970b00d2da851ba
Ray Stevens <ray@rastevens.me>
2021-04-16 22:32:24 -0400

    Added server-side-accessible manual for users

--- init-bot.py
+++ init-bot.py
@@ -43,4 +43,18 @@ async def on_message(message):
     if message.content.startswith('$end'):
         await message.channel.send('I understand combat has ended.')
 
+    if message.content.startswith('$manual'):
+        await message.channel.send("""I'm here to help you keep track of combat initiative, and I understand the following commands:
+
+        $hello — I'll say hello back!
+
+        $i — Command to add someone to my initiative tracker list. After the $i, specify character name, iniative bonus, and whether they get advantage on the roll (if they do, say "adv"; if they don't, you don't need to say so.) Example: "$i Fiver adv" means that Fiver gets +5 to initiative rolls and has advantage on initiative rolls; "$i Fiver 5" means she has +5 but no initiative. Enter this command once per character, per combat.
+
+        $roll — Command to have me take all of the characters you've entered, roll their initiative for you, and put them in order. I'll pin the post to make it easy to reference during combat.
+
+        $new-challenger — Command to insert a new character mid-combat. I'm still learning and initiative order is a lot to remember, so you'll need to roll for the newcomer. After the $new-challenger, specify character name and what they rolled, like this: "$new-challenger Lich Queen 17". I'll update the post to match.
+
+        $end — Command to end combat. I'll un-pin the post and wipe the order clean for next time.
+        """)
+
 client.run(TOKEN)
