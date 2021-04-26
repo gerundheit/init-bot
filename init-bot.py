89fc0df2dabd1c4cba31bfb060334952d958c2cf
Ray Stevens <ray@rastevens.me>
2021-04-25 23:18:45 -0400

    Can now remove characters from initiative order before or after rolling

--- init-bot.py
+++ init-bot.py
@@ -65,6 +65,7 @@ async def on_message(message):
         output = '\n'.join(output)
         await message.channel.send(output)
 
+#Rolls an additional character into the initiative order mid-combat
     if message.content.startswith('$add'):
         command = str(message.content).split()
         name, bonus = command[1], int(command[2])
@@ -84,7 +85,30 @@ async def on_message(message):
         output = '\n'.join(output)
         await message.channel.send(output)
 
-    #Dice rolls
+#Removes a character from the initiative order, pre-rolling or mid-combat
+    if message.content.startswith('$del'):
+        command = str(message.content).split()
+        name = command[1]
+        for tup in initiative_entries: #Deletes from the pre-roll initiative roster
+            if name in str(tup):
+                initiative_entries.remove(tup)
+        for tup in rolled_initiative_values: #Deletes from the rolled initiative order variable
+            if name in str(tup):
+                rolled_initiative_values.remove(tup)
+        await message.channel.send('Removed {} from combat.'.format(name))
+        if rolled_initiative_values != []:  #Re-sends the updated initiative order to the text channel if combat has already started
+            rolled_initiative_values.sort(key=lambda a: a[1], reverse=True)
+            output = ['{}: {}'.format(result[0], result[1]) for result in rolled_initiative_values]
+            output = '\n'.join(output)
+            await message.channel.send(output)
+
+#Clears the initiative order from the list variable and unpins the previous message for a fresh start
+    if message.content.startswith('$end'):
+        await message.channel.send('Combat has ended. Clearing the board.')
+        del rolled_initiative_values[0:]
+        del initiative_entries[0:]
+
+#Misc dice rolls
     if message.content.startswith('$roll'):
         command = str(message.content).split()
         roll_bonus = command[1].split('+')
@@ -102,12 +126,6 @@ async def on_message(message):
         rolls_sum = rolls_sum + roll_bonus
         await message.channel.send('That\'s {} ({}+{}).'.format(rolls_sum, results_list, roll_bonus))
 
-    #Clears the initiative order from the list variable and unpins the previous message for a fresh start
-    if message.content.startswith('$end'):
-        await message.channel.send('Combat has ended. Clearing the board.')
-        del rolled_initiative_values[0:]
-        del initiative_entries[0:]
-
 #Displays help text/manual in the text channel.
     if message.content.startswith('$manual'):
         await message.channel.send("""I'm here to roll dice and to help you keep track of combat initiative, and I understand the following commands:
@@ -121,6 +139,8 @@ async def on_message(message):
         $go : Command to have me take all of the characters you've entered, roll their initiative for you, and put them in order. I'll pin the post to make it easy to reference during combat.
 
         $add : Command to insert a new character mid-combat. Use the same arguments for names, bonuses, and advantage as you do with the $i command.
+
+        $del : Command to delete a character from the initiative order. This works before or after combat begins (AKA before or after the $go command). Just specify character name, e.g. "$del Fiver".
 
         $end : Command to end combat. I'll wipe the order clean for next time, but you need to un-pin the old post, please.
         """)
